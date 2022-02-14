8258f0180610ad2ca2b3cc3f37d61a6326638ae7
Jordan McMichael <jlmcmchl@gmail.com>
2022-02-13 23:34:43 -0500

    cpu _and_ memory efficient now

--- slice_enable_periods.py
+++ slice_enable_periods.py
@@ -1,137 +1,172 @@
 import csv
 import argparse
 from collections import defaultdict
-
-def contained_by_any_range(val, vec):
-    for (start, end) in vec:
-        if start <= val <= end:
-            return True
-    return False
-
-def contained_by_range(val, range):
-    return range[0] <= val <= range[1]
+from timeit import default_timer as timer
+import bisect
 
 
-def read_csv(path, condition, toggle):
-    with open(path, newline='') as f:
+def binarySearch(alist, item, key):
+    first = 0
+    last = len(alist) - 1
+    found = False
+
+    while first <= last and not found:
+        pos = 0
+        midpoint = (first + last) // 2
+        val = key(alist[midpoint])
+        if val == item:
+            pos = midpoint
+            found = True
+        else:
+            if item < val:
+                # print(f'{item} < {val}, searching before {midpoint}')
+                last = midpoint - 1
+            else:
+                # print(f'{item} > {val}, searching after {midpoint}')
+                first = midpoint + 1
+    if not found:
+        # first_key = key(alist[first])
+        # last_key = key(alist[last])
+        # print(f'found these two: {first_key} and {last_key}')
+        return (first, found)
+    return (pos, found)
+
+
+def read_csv(args):
+    with open(args.input, newline="", buffering=1) as f:
         reader = csv.DictReader(f)
-        state = defaultdict(str)
         ranges = []
 
-        events_log = [(False, 0)]
+        candidate_rows = []
+        rows = []
 
-        last_eval = False
+        active_window = False
+        was_active_window = False
+        min_time = 0
+        max_time = 0
+        keys = reader.fieldnames
 
         for row in reader:
-            for key in row:
-                if row[key] != '':
-                    state[key] = row[key]
-            
-            if toggle:
-                evaluated_condition = eval(condition)
+            # if on_condition, add all rows after before_window to actual rows
+            # then just add row to rows until off_condition
+            # then set max timestamp and add to `rows` until that condition is met or until on_condition returns true
+            time = float(row["timestamp"])
 
-                if evaluated_condition and not last_eval:
-                    print('enabled at', row['timestamp'])
-                    events_log.append((True, float(row['timestamp'])))
-                    last_eval = True
-                elif not evaluated_condition and last_eval:
-                    print('disabled at', row['timestamp'])
-                    events_log.append((False, float(row['timestamp'])))
-                    last_eval = False
+            if eval(args.begin_condition) and not active_window:
+                print("on_condition returned true at", time)
+
+                if not was_active_window:
+                    min_time = max(0, time - args.before)
+
+                    candidate_min_time = candidate_rows[0]['timestamp']
+                    candidate_max_time = candidate_rows[-1]['timestamp']
+
+                    if min_time < float(candidate_min_time):
+                        rows.extend(candidate_rows)
+                    elif min_time > float(candidate_max_time):
+                        pass
+                    else:
+                        # it's somewhere in between
+                        print(f'binary searching for {min_time} between {candidate_min_time} and {candidate_max_time}')
+
+                        (candidate_min_index, _) = binarySearch(
+                            candidate_rows,
+                            min_time,
+                            lambda row: float(row["timestamp"]),
+                        )
+
+                        rows.extend(candidate_rows[candidate_min_index:])
+                        
+                    rows.append(row)
+
+                    del candidate_rows
+                    candidate_rows = []
+
+                active_window = True
+                was_active_window = False
+
+            elif eval(args.end_condition) and active_window:
+                print("off_condition returned true at", time)
+                active_window = False
+                was_active_window = True
+                max_time = time + args.after
+
+            if active_window or time < max_time:
+                rows.append(row)
             else:
-                if eval(condition):
-                    ranges.append((float(row['timestamp']), float(row['timestamp'])))
-            rows.append(row)
-        
-        if toggle:
-            start_time = 0
+                candidate_rows.append(row)
 
-            for event in events_log:
-                if event[0]:
-                    start_time = event[1]
-                else:
-                    if start_time != 0:
-                        # start_time = 0 is always during disabled
-                        ranges.append((start_time, event[1]))
-                        start_time = 0
-            
-            # if we're still enabled at the very end, extend enable range to the end
-            if start_time != 0:
-                ranges.append((start_time, float(row['timestamp'])))
-        keys = [*row.keys()]
-    
+            if was_active_window and time > max_time:
+                print(f"adding range of {min_time}-{max_time}")
+                write_file(args.output, rows, keys)
+                del(rows)
+                rows = []
+                was_active_window = False
+
+    if len(rows) > 0:
+        print(f"adding range of {min_time}-{max_time}")
+        write_file(args.output, rows, keys)
+        del(rows)
+    del(candidate_rows)
     return (rows, keys, ranges)
 
 
-def compress_ranges(ranges):
-    compressed_ranges = []
+def write_file(output, rows, keys):
+    print("writing rows:", len(rows))
 
-    for (start, end) in ranges:
-        if len(compressed_ranges) == 0:
-            compressed_ranges.append((max(start-args.before, 0), end+args.after))
-        else:
-            if start - args.before < compressed_ranges[-1][1]:
-                compressed_ranges[-1] = (compressed_ranges[-1][0], end + args.after)
-            else:
-                compressed_ranges.append((max(start-args.before, 0), end+args.after))
+    fname = output + "_" + rows[0]["timestamp"] + "-" + rows[-1]["timestamp"] + ".csv"
 
-
-    return compressed_ranges
-
-
-def write_files(output, rows, keys, multiple, ranges):
-    if multiple: 
-        rows_index = 0
-        for (ind, range) in enumerate(ranges):
-            with open(output + '_' + str(range[0]) + '-' + str(range[1]) + '.csv', 'w', newline='') as f:
-                print(f'writing file {ind}, range={range}')
-                writer = csv.DictWriter(f, fieldnames=keys)
-                writer.writeheader()
-
-                was_in_range = False
-                while rows_index < len(rows):
-                    row = rows[rows_index]
-                    in_range = contained_by_range(float(row['timestamp']), range)
-                    if was_in_range and not in_range:
-                        break
-                    elif in_range:
-                        was_in_range = True
-                        writer.writerow(row)
-                    rows_index += 1
-    else:
-        with open(output + '.csv', 'w', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=keys)
-            writer.writeheader()
-
-            for row in rows:
-                if contained_by_any_range(float(row['timestamp']), ranges):
-                    writer.writerow(row)
+    with open(fname, "w", newline="", buffering=1) as f:
+        print(f"writing file {fname}")
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(rows)
+        f.flush()
+    print("write complete")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Extract Enable Periods from wpilog CSV exports. Multiple overlapping windows are treated as one.")
-    parser.add_argument('-i', '--input', help='input CSV file representing a WPILog')
-    parser.add_argument('-o', '--output', help='output filename to write filtered results to')
-    parser.add_argument('-B', '--before',  help='seconds before enable period to include', default=10)
-    parser.add_argument('-A', '--after',  help='seconds after enable period to include', default=10)
-    parser.add_argument('-m', '--multiple', action='store_true', help='output one file per range instead of one total. NOT IMPLEMENTED')
-    parser.add_argument('-c', '--condition', default="state['DS:enabled'] == '1'", help='condition to check for defining your logging window')
-    parser.add_argument('-t', '--no-toggle', action='store_false', help='if set condition specifies an event. if false, condition specifies a window')
+    start = timer()
+    parser = argparse.ArgumentParser(
+        description="Extract Enable Periods from wpilog CSV exports. Multiple overlapping windows are treated as one."
+    )
+    parser.add_argument("-i", "--input", help="input CSV file representing a WPILog")
+    parser.add_argument(
+        "-o", "--output", help="output filename to write filtered results to"
+    )
+    parser.add_argument(
+        "-B", "--before", help="seconds before enable period to include", default=10
+    )
+    parser.add_argument(
+        "-A", "--after", help="seconds after enable period to include", default=10
+    )
+    parser.add_argument(
+        "-m",
+        "--multiple",
+        action="store_true",
+        help="output one file per range instead of one total. NOT IMPLEMENTED",
+    )
+    parser.add_argument(
+        "-b",
+        "--begin-condition",
+        default="row['DS:enabled'] == '1'",
+        help="when this is true, begin window",
+    )
+    parser.add_argument(
+        "-e",
+        "--end-condition",
+        default="row['DS:enabled'] == '0'",
+        help="when this is true, end window",
+    )
 
     args = parser.parse_args()
     args.before = int(args.before)
     args.after = int(args.after)
 
-    if args.output[-4:] == '.csv':
+    if args.output[-4:] == ".csv":
         args.output = args.output[:-4]
 
     keys = []
-    rows = []
 
-    (rows, keys, ranges) = read_csv(args.input, args.condition, args.no_toggle)
-    
-    ranges = compress_ranges(ranges)
-
-    write_files(args.output, rows, keys, args.multiple, ranges)
-
-    
+    (rows, keys, ranges) = read_csv(args)
+    print(f"completed in {timer() - start} seconds")
